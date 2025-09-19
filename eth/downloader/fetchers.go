a9b2e60ad2c057b6fdad4ada64bc0d49a7e19ad7
steve-nexus <steve@to.nexus>
2025-09-19 18:03:06 +0900

    add ttl to log

--- eth/downloader/fetchers.go
+++ eth/downloader/fetchers.go
@@ -54,7 +54,7 @@ func (d *Downloader) fetchHeadersByHash(p *peerConnection, hash common.Hash, amo
 		p.log.Debug("Header request timed out", "elapsed", ttl)
 		headerTimeoutMeter.Mark(1)
 
-		log.Warn("Timeout peer", "id", p.id, "method", "fetchHeadersByHash")
+		log.Warn("Timeout peer", "id", p.id, "elapsed", ttl, "method", "fetchHeadersByHash")
 		return nil, nil, errTimeout
 
 	case res := <-resCh:
@@ -100,7 +100,7 @@ func (d *Downloader) fetchHeadersByNumber(p *peerConnection, number uint64, amou
 		p.log.Debug("Header request timed out", "elapsed", ttl)
 		headerTimeoutMeter.Mark(1)
 
-		log.Warn("Timeout peer", "id", p.id, "method", "fetchHeadersByNumber")
+		log.Warn("Timeout peer", "id", p.id, "elapsed", ttl, "method", "fetchHeadersByNumber")
 		return nil, nil, errTimeout
 
 	case res := <-resCh:
