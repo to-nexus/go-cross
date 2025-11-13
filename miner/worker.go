a14e679eb793f3341e499bded48f4bb391bb96d2
deekay-nexus <dkshin@to.nexus>
2025-11-13 17:21:46 +0900

    Fix/recommit timer and far-future message handling (#50)
    - Update far future message handling:
      - Loosen check range for far future message
    
    - Update recommit timer:
      - Update worker's default recommit duration to 10 seconds

--- miner/worker.go
+++ miner/worker.go
@@ -514,11 +514,6 @@ func (w *worker) newWorkLoop(recommit time.Duration) {
 			commit(commitInterruptNewHead)
 
 		case <-timer.C:
-			// ##CROSS: istanbul
-			if w.chainConfig.Istanbul != nil {
-				continue
-			}
-			// ##
 
 			// If sealing is running resubmit a new work cycle periodically to pull in
 			// higher priced transactions. Disable this overhead for pending blocks.
